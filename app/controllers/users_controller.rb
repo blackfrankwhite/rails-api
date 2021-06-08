f8f85ccdd97938c29009fd776792807d9a960596
root <gugakhachvani@gmail.com>
2021-06-08 11:43:28 +0000

    add registr in controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,24 +3,29 @@ class UsersController < ApplicationController
   
     # REGISTER
     def create
-      @user = User.create(user_params)
-      if @user.valid?
-        token = encode_token({user_id: @user.id})
-        render json: {user: @user, token: token}
+      if params[:password] != params[:password2]
+        render json: {error: "password do not mathc"}
       else
-        render json: {error: "Invalid username or password"}
+        @user = User.create(email:params[:email],password:params[:password],mark_for_deletion:'f')            
+        if @user.valid?
+          token = encode_token({user_id: @user.id})
+          render json: {user: @user, token: token}
+        else
+          render json: {error: "Invalid email or password"}
+        end
       end
     end
   
     # LOGGING IN
     def login
+      puts 'aqine iqine'
       @user = User.find_by(email: params[:email])
   
       if @user && @user.authenticate(params[:password])
         token = encode_token({user_id: @user.id})
         render json: {user: @user, token: token}
       else
-        render json: {error: "Invalid username or password"}
+        render json: {error: "Invalid email or password"}
       end
     end
   
