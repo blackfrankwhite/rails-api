f49a3ccb006ed0d6b1f84ce04d525c6ea820270c
root <gugakhachvani@gmail.com>
2021-06-08 11:41:32 +0000

    changes on db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_07_135800) do
+ActiveRecord::Schema.define(version: 2021_06_08_093958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.string "desc"
+    t.string "status_state"
+    t.string "testing"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -23,4 +33,5 @@ ActiveRecord::Schema.define(version: 2021_06_07_135800) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "todos", "users"
 end
