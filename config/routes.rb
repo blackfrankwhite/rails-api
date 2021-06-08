6838268b5ac2807a4539f68046e6217adeb6a937
root <gugakhachvani@gmail.com>
2021-06-08 11:42:26 +0000

    adding sign uo rout

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resource :users, only: [:create]
   post "/login", to: "users#login"
+  post "/sign_up", to: "users#create"
   get "/auto_login", to: "users#auto_login"
 end
